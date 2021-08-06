b7f8a97bddf88e75cf77e82605f4c522c10c6b92
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2021-08-06 13:51:39 -0600

    refactor: switch to typescript

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import {
   getInput, setOutput, setFailed, info, group,
 } from '@actions/core';
 import { context } from '@actions/github';
-
-const { Octokit } = require('@octokit/action');
+import { Octokit } from '@octokit/action';
 
 const octokit = new Octokit();
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/');
+if (!owner || !repo) {
+  setFailed('GITHUB_REPOSITORY environment variable not present or correct.');
+  process.exit(1);
+}
 
 const head = getInput('head');
 const base = getInput('base');
@@ -20,16 +23,15 @@ info(`Firing from ${context.eventName} on ${context.ref}`);
 try {
   group('pull-request', async () => {
     info('Checking if open pull request for branch already exists...');
+
     const { data } = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
       owner,
       repo,
       head: owner.concat(':', head),
       state: 'open',
-    }).catch((error) => {
-      setFailed(error.message);
     });
 
-    let pullNumber = null;
+    let pullNumber = 0;
 
     if (data.length > 0) {
       pullNumber = data[0].number;
@@ -41,8 +43,6 @@ try {
         pull_number: pullNumber,
         title,
         body,
-      }).catch((error) => {
-        setFailed(error.message);
       }).then(() => {
         info(`Updated #${pullNumber}`);
       });
@@ -56,22 +56,18 @@ try {
         head,
         base,
         draft,
-      }).catch((error) => {
-        setFailed(error.message);
       }).then((response) => {
         pullNumber = response.data.number;
         setOutput('pull-number', pullNumber);
         info(`Created #${pullNumber}`);
       });
     }
-    if (assignee) {
+    if (assignee && pullNumber) {
       await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/assignees', {
         owner,
         repo,
         issue_number: pullNumber,
         assignees: [assignee],
-      }).catch((error) => {
-        setFailed(error.message);
       });
     }
   });

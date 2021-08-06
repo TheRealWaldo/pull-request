b531c0e01298ece1f41e4f49345313b9bbe05ff8
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2021-08-06 14:23:25 -0600

    chore: fix eslint and add vscode extensions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import {
-  getInput, setOutput, setFailed, info, group,
-} from '@actions/core';
+import { getInput, setOutput, setFailed, info, group } from '@actions/core';
 import { context } from '@actions/github';
 import { Octokit } from '@octokit/action';
 
@@ -14,11 +12,11 @@ if (!owner || !repo) {
 info(`Firing from ${context.eventName} on ${context.ref}`);
 
 try {
-  const head = getInput('head', {required: true});
-  const base = getInput('base', {required: true});
-  const title = getInput('title', {required: true});
-  const body = getInput('body', {required: true});
-  const draft = (getInput('draft') === 'true');
+  const head = getInput('head', { required: true });
+  const base = getInput('base', { required: true });
+  const title = getInput('title', { required: true });
+  const body = getInput('body', { required: true });
+  const draft = getInput('draft') === 'true';
   const assignee = getInput('assignee');
 
   group('pull-request', async () => {
@@ -37,30 +35,34 @@ try {
       pullNumber = data[0].number;
       setOutput('pull-number', pullNumber);
       info(`PR #${pullNumber} exists, updating...`);
-      await octokit.request('PATCH /repos/{owner}/{repo}/pulls/{pull_number}', {
-        owner,
-        repo,
-        pull_number: pullNumber,
-        title,
-        body,
-      }).then(() => {
-        info(`Updated #${pullNumber}`);
-      });
+      await octokit
+        .request('PATCH /repos/{owner}/{repo}/pulls/{pull_number}', {
+          owner,
+          repo,
+          pull_number: pullNumber,
+          title,
+          body,
+        })
+        .then(() => {
+          info(`Updated #${pullNumber}`);
+        });
     } else {
       info('PR does not already exist, creating...');
-      await octokit.request('POST /repos/{owner}/{repo}/pulls', {
-        owner,
-        repo,
-        title,
-        body,
-        head,
-        base,
-        draft,
-      }).then((response) => {
-        pullNumber = response.data.number;
-        setOutput('pull-number', pullNumber);
-        info(`Created #${pullNumber}`);
-      });
+      await octokit
+        .request('POST /repos/{owner}/{repo}/pulls', {
+          owner,
+          repo,
+          title,
+          body,
+          head,
+          base,
+          draft,
+        })
+        .then((response) => {
+          pullNumber = response.data.number;
+          setOutput('pull-number', pullNumber);
+          info(`Created #${pullNumber}`);
+        });
     }
     if (assignee && pullNumber) {
       await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/assignees', {
